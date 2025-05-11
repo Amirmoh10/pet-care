f611f49c97b07d5d6958ed332db6a7164773623b
Amir Ghezala <mohamedghezala@gmail.com>
2025-05-11 16:02:51 +0400

    add petlist and petdetails

---
+++ src/components/pet-list.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+const PetList = () => {
+  return (
+    <ul className="bg-white border-b border-black/[0.08]">
+      <li>
+        <button
+          className={
+            "flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition"
+          }
+        >
+          <Image
+            src={"/pet-placeholder.png"}
+            alt="Pet image"
+            width={45}
+            height={45}
+            className="w-[45px] h-[45px] rounded-full object-cover"
+          />
+          <p className="font-semibold">test</p>
+        </button>
+      </li>
+    </ul>
+  );
+};
+
+export default PetList;
