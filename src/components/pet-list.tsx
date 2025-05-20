82fca0836704d92c4dc6f58b13a5bf4978256f65
Amir Ghezala <mohamedghezala@gmail.com>
2025-05-20 21:21:44 +0400

    fetch pets

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,24 +1,37 @@
 import Image from "next/image";
+import { Pet } from "@/lib/utils";
 
-const PetList = () => {
+const PetList = async () => {
+  const response = await fetch(
+    "https://bytegrad.com/course-assets/projects/petsoft/api/pets"
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch pets");
+  }
+
+  const pets = await response.json();
+
   return (
     <ul className="bg-white border-b border-black/[0.08]">
-      <li>
-        <button
-          className={
-            "flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition"
-          }
-        >
-          <Image
-            src={"/pet-placeholder.png"}
-            alt="Pet image"
-            width={45}
-            height={45}
-            className="w-[45px] h-[45px] rounded-full object-cover"
-          />
-          <p className="font-semibold">test</p>
-        </button>
-      </li>
+      {pets.map((pet: Pet) => (
+        <li key={pet.id}>
+          <button
+            className={
+              "flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition"
+            }
+          >
+            <Image
+              src={pet.imageUrl}
+              alt="Pet image"
+              width={45}
+              height={45}
+              className="w-[45px] h-[45px] rounded-full object-cover"
+            />
+            <p className="font-semibold">{pet.name}</p>
+          </button>
+        </li>
+      ))}
     </ul>
   );
 };
